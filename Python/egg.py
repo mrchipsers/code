ed773e46ffdc69ef7b58c1e06283d5d3148bed9e
Sofia <sachalouro@icloud.com>
2025-09-25 21:03:30 -0400

    tested static variables in java

--- Python/egg.py
+++ Python/egg.py
@@ -1,15 +1,20 @@
 def eggs(n):
     #boxes=-(n//-6)
-    #boxes=((n+(6-1))//6)
-    #empty=-(n%-6)
-    #boxes=(n//6)+1 
     boxes = (n+5)//6
     #or
-    #empty=(boxes*6)-n
-    #empty=(boxes*6)-n
-    #empty=empty%6
-    empty = (6-(n%6))%6
-    return f"there are {boxes} boxes, and {empty} spots in the last box"
-egg=int(input("please input a number of eggs: "))
-print(eggs(egg))
-    
+    #empty=-(n%-6)
+    empty=(boxes*6)-n
+    #empty = (6-(n%6))%6
+    if boxes<2:
+        box= "there is 1 box"
+    else:
+        box= f"there are {boxes} boxes"
+    if empty==1:
+        am= "there is 1 empy spot in the last box"
+    else:
+        am= f"there are {empty} empty spots in the last box"
+    return f"for {n} eggs {box}, and {am}"
+#egg=int(input("please input a number of eggs: "))
+#print(eggs(egg))
+for i in range(5,15):
+    print(eggs(i))
