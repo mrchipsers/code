ede464da491240b049c37859f887f39df4091ee7
Sofia <sachalouro@icloud.com>
2025-09-30 17:45:15 -0400

    took into acount time in the decision

--- Java/TripEvaluator.java
+++ Java/TripEvaluator.java
@@ -7,23 +7,58 @@ public class TripEvaluator{
         System.out.println("how much does it cost to take the ferry? (if you aren't taking the ferry or there is no cost, input 0)");
         double ferry = input.nextDouble();
         input.close();
+        
         if (distance<0) {
             System.out.println("Invalid Input");
             return;
         }
-        if 
         
+        double[] car = calculateCarCost(distance, ferry);
+        double carcost = car[0];
+        double cartime = car[1];
+        double cscore=  carcost+cartime;
+        
+        double[] plane = calculateFlightCost(distance);
+        double planecost = plane[0];
+        double planetime = plane[1];
+        double pscore = planecost+planetime;
+       
+        double[] train = calculateTrainCost(distance);
+        double traincost = train[0];
+        double traintime = train[1];
+        double tscore = traincost+traintime;
+       
+        if (cscore<pscore && cscore<tscore){
+            System.out.println("You should take the car. it costs "+carcost+" and will take "+cartime+" hours");
+        }else if (pscore<cscore && pscore<tscore){
+            System.out.println("You should take the plane. it costs "+planecost+" and will take "+planetime+" hours");
+        }else{
+            System.out.println("You should take the train. it costs "+traincost+" and will take "+traintime+" hours");
+        }
     }
-    public static double calculateFlightCost(double distance){
+
+    public static double[] calculateFlightCost(double distance){
         double cost = (distance*0.2)+200;
-        return cost;
+        double time = (distance/900)+3;
+        time = ((int)(time*100))/100.0;
+        double[] list = {cost, time};
+        return list;
     }
-    public static double calculateCarCost(double distance, double ferry){
+
+    public static double[] calculateCarCost(double distance, double ferry){
         double cost = (distance*0.4)+ferry;
-        return cost;
+        double time = distance/100;
+        time = ((int)(time*100))/100.0;
+        double[] list = {cost, time};
+        return list;
     }
-    public static double calculateTrainCost(double distance){
+    
+    public static double[] calculateTrainCost(double distance){
         double cost = distance*0.25;
-        return cost;
+        double time = (distance/350)+0.5;
+        time = ((int)(time*100))/100.0;
+        double[] list = {cost, time};
+        return list;
+        
     }
 }
