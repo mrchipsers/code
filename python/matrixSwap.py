24ea01cd9d9163e3f3e0e192cb2b55023f832f25
Sofia <sachalouro@icloud.com>
2026-02-23 10:12:40 -0500

    sorting lists of lists

--- python/matrixSwap.py
+++ python/matrixSwap.py
@@ -5,3 +5,19 @@ def swapMatrix(matrix: list[list]):
     return matrix
 
 print(swapMatrix([[1,2,3],[4,5,6],[7,8,9]]))
+
+def sortListList(lst: list):
+    for i, val in enumerate(lst):
+        lst[i]=sort(val)
+    return lst
+
+def sort(list):
+    for i in range(len(list)):
+        smallest_index=i
+        for j in range(i,len(list)):
+            if list[j]>list[smallest_index]:
+                smallest_index=j
+        list[i], list[smallest_index]= list[smallest_index],list[i]
+    return list
+
+print(sortListList([[1,3,2],[5,4,6],[9,8,7]]))
